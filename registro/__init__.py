e01228db8404a90d8f87f7303df83402c7d3cb21
Fluidone Morinmoto <morin@fluidone.it>
2020-06-07 17:47:02 +0200

    Closes #517 @0.25 Implemented a simple log system

--- registro/__init__.py
+++ registro/__init__.py
@@ -1,3 +1,11 @@
+import logging
+
 default_app_config = 'registro.apps.RegistroConfig'
 
 APP_VERSION = 'beta-dev'
+__version__ = APP_VERSION
+__app_alias__ = 'Registro segna ore'
+
+logger = logging.getLogger('base')
+
+logger.debug("Starting '{}' v. {}".format(__app_alias__, __version__))
